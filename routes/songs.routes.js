7bb8d52650550154cc9747161c4f7b2d19ec209c
Jonatan Iglesias <jonathan-18719@outlook.es>
2023-07-05 14:05:14 +0200

    actualizando votos

--- routes/songs.routes.js
+++ routes/songs.routes.js
@@ -6,6 +6,17 @@ router.get("/", (req, res) => {
   res.json(songsData);
 });
 
+router.put("/vote/:songId", (req,res)=>{
+  const songId = req.params.songId;
+  const response = songsData.find((eachSong) => eachSong.id===songId);
+  if(response){
+    eachSong.votos += 1
+    res.json({message: "Voto Agregado"})
+  }else{
+    res.status(404).json({message: "Canción no encontrada"})
+  }
+})
+
 // hacer una ruta individual para hacer la llamada a yt cuando se vaya a reproducir el video
 
 module.exports = router;
